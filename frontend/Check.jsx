ac9a65a5871e78eb85ba03b26b70ee47b66aa421
DanielBizualem <136451770+DanielBizualem@users.noreply.github.com>
2025-01-24 13:18:45 +0300

    Update Check.jsx

--- frontend/Check.jsx
+++ frontend/Check.jsx
@@ -1,14 +1,1 @@
-import React from 'react'
-import { products } from './src/assets/assets/frontend_assets/assets'
 
-const Check = () => {
-  const applyFilter = (e)=>{
-    let copyProduct = products.slice()
-    copyProduct = copyProduct.filter(item=>category.include(e.target.value))
-  }
-  return (
-    <div>Check</div>
-  )
-}
-
-export default Check
